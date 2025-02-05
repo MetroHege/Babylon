27ef8af23f46ddf69ceb9c87214623ae0664eab8
MetroHege <113589548+MetroHege@users.noreply.github.com>
2025-02-05 10:30:44 +0200

    add modal for video playback and configure MIME type in .htaccess

--- src/main.ts
+++ src/main.ts
@@ -35,30 +35,24 @@ const createScene = () => {
   console.log(ground);
 
   // Load first splat
-  SceneLoader.ImportMeshAsync(
-    "splat",
-    "./public/",
-    "untitled.splat",
-    scene
-  ).then((result) => {
-    const splat1 = result.meshes[0];
-    splat1.position = new Vector3(-1.5, 5, 1.5);
-    splat1.scaling = new Vector3(2.0, 2.0, 2.0); // Adjust the scale as needed
-    splat1.rotation = new Vector3(0, Math.PI / 4, 0); // Adjust the rotation as needed
-  });
+  SceneLoader.ImportMeshAsync("splat", "./", "untitled.splat", scene).then(
+    (result) => {
+      const splat1 = result.meshes[0];
+      splat1.position = new Vector3(-1.5, 5, 1.5);
+      splat1.scaling = new Vector3(2.0, 2.0, 2.0); // Adjust the scale as needed
+      splat1.rotation = new Vector3(0, Math.PI / 4, 0); // Adjust the rotation as needed
+    }
+  );
 
   // Load second splat
-  SceneLoader.ImportMeshAsync(
-    "splat",
-    "./public/",
-    "clstesti.splat",
-    scene
-  ).then((result) => {
-    const splat2 = result.meshes[0];
-    splat2.position = new Vector3(2, 1.35, 0);
-    splat2.scaling = new Vector3(0.5, 0.5, 0.5); // Adjust the scale as needed
-    splat2.rotation = new Vector3(0, Math.PI / 4, 0); // Adjust the rotation as needed
-  });
+  SceneLoader.ImportMeshAsync("splat", "./", "clstesti.splat", scene).then(
+    (result) => {
+      const splat2 = result.meshes[0];
+      splat2.position = new Vector3(2, 1.35, 0);
+      splat2.scaling = new Vector3(0.5, 0.5, 0.5); // Adjust the scale as needed
+      splat2.rotation = new Vector3(0, Math.PI / 4, 0); // Adjust the rotation as needed
+    }
+  );
 
   return scene;
 };
@@ -71,3 +65,22 @@ engine.runRenderLoop(function () {
 window.addEventListener("resize", function () {
   engine.resize();
 });
+
+// Modal functionality
+const modal = document.getElementById("myModal") as HTMLElement;
+const btn = document.getElementById("openModalBtn") as HTMLElement;
+const span = document.getElementsByClassName("close")[0] as HTMLElement;
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
